b6165939d19c634ce22cfaebd2c16464546ee50f
Emirhan Dinc <Sinojin24@gmail.com>
2016-11-18 01:26:24 +0300

    --news ekleme de yapildi.

--- back-end/models/news.go
+++ back-end/models/news.go
@@ -1,16 +1,47 @@
 package models
 
-import "github.com/astaxie/beego/orm"
+import (
+	"github.com/astaxie/beego/orm"
+	"errors"
+)
+
+var missingAtribute = errors.New("Haber eklerken eksik bilgi var")
+
 
 type News struct {
-	Id         int `orm:"auto;serial"`
+	Id      int `orm:"auto;serial"`
 	Title   string `orm:"size(30);null"`
-	Content   string `orm:"size(20);null"json:"-"`
-	Userid   int `orm:"size(20);null"`
-	Rate   int `orm:"size(2);null"`
-	Status int `orm:"size(2);null"`
+	Content string `orm:"size(20);null"json:"-"`
+	Userid  int `orm:"size(20);null"`
+	Rate    int `orm:"size(2);null"`
+	Status  int `orm:"size(2);null"`
 }
 
 func init() {
 	orm.RegisterModel(new(News))
+}
+
+//to save news
+func (this *News) save() error {
+	if (this.Content == "") || (this.Title == "" ){
+		return missingAtribute
+	}
+
+	o := orm.NewOrm();
+	o.Insert(this)
+
+	return nil
+}
+
+
+//update
+func (this *News) Update()  {
+	if (this.Content == "") || (this.Title == "" ){
+		return missingAtribute
+	}
+
+
+	o := orm.NewOrm();
+	o.Update(this)
+
 }
